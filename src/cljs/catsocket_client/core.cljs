5cca8254a47834e233fdcbedeacde1cc1a25aa8f
Jakub Arnold <darthdeus@gmail.com>
2015-04-27 09:25:29 +0200

    Add identify, join, leave & broadcast

--- src/cljs/catsocket_client/core.cljs
+++ src/cljs/catsocket_client/core.cljs
@@ -13,16 +13,6 @@
 
 (defn stringify [msg] (.stringify js/JSON (clj->js msg)))
 
-(defn main []
-  (om/root
-    (fn [app owner]
-      (reify
-        om/IRender
-        (render [_]
-          (dom/h1 nil (str "nuf " (:text app))))))
-    app-state
-    {:target (. js/document (getElementById "app"))}))
-
 (defn s4 []
   (let [r (+ 1 (Math/random))
         r2 (* r 0x10000)
@@ -62,12 +52,19 @@
                 :timestamp (.getTime (js/Date.))}]
     (if @connected?
       (do
+        (log "sending via socket, waiting for ACK")
         (swap! sent-messages assoc (:id params) params)
         (.send socket (stringify params)))
-      (swap! queue conj params))))
+      (do
+        (log "enqueing")
+        (swap! queue conj params)))))
 
 (defn flush-queue []
-  (log "flushing queue"))
+  (log "identified flushing queue")
+  (doseq [item @queue]
+    (log "sending" (clj->js item))
+    (.send @socket (stringify item)))
+  (reset! queue []))
 
 (defn on-message [data]
   (condp = (.-action data)
@@ -78,3 +75,30 @@
                 (flush-queue))
               (swap! sent-messages dissoc id)))
     (log "unrecognized message" data)))
+
+(defn join [room]
+  (send @socket "join" {:room room}))
+
+(defn leave [room]
+  (send @socket "leave" {:room room}))
+
+(defn broadcast [room msg]
+  (send @socket "broadcast" {:room room :message msg}))
+
+;;
+
+(defn main []
+  (init)
+  (join "test")
+  (broadcast "test" "hello!")
+
+
+
+  (om/root
+    (fn [app owner]
+      (reify
+        om/IRender
+        (render [_]
+          (dom/h1 nil (str "nuf " (:text app))))))
+    app-state
+    {:target (. js/document (getElementById "app"))}))
